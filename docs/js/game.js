f3703f020c57410a85199d64d17a801885fbe239
negi141 <takuto.negishi@gmail.com>
2022-08-26 00:08:25 +0900

    fix

--- docs/js/game.js
+++ docs/js/game.js
@@ -24,8 +24,8 @@ export class Game {
         element: document.body,
         engine: engine,
         options: {
-            width: 375,
-            height: 667,
+            width: 575,
+            height: 867,
             //showAngleIndicator: true,
             wireframes: false,
             background: '#223344',
@@ -49,14 +49,24 @@ export class Game {
             parts: [partA]
         });
     });*/
+    /*
+    var stack = Composites.stack(300, 600 - size * 6/2, 5, 5, 0, 0, function(x, y) {
+        var partA = Bodies.rectangle(x, y, size, size, { label: 'block', density: 0.5, restitution: 0.000000001 });
+
+        return Body.create({
+            parts: [partA]
+        });
+    });*/
+
     Common.setDecomp(window.decomp);
+
     var blockTemplates = [
-        Vertices.fromPath('0 0  50 0  50 50  0 50'),
-        Vertices.fromPath('0 0  100 0  100 50  0 50'),
-        Vertices.fromPath('0 0  100 0  100 100  0 100'),
-        Vertices.fromPath('0 0  50 0  50 50 100 50 100 100 0 100'),
-        Vertices.fromPath('0 0  50 0  50 100 0 100'),
-        Vertices.fromPath('0 0  50 0  50 50  100 50  100 0  150 0  150 100  0 100'),
+        {vert:Vertices.fromPath('0 0  50 0  50 50  0 50'), color:'#FF6B22'},
+        {vert:Vertices.fromPath('0 0  100 0  100 50  0 50'), color:'#005AFF'},
+        {vert:Vertices.fromPath('0 0  100 0  100 100  0 100'), color:'#03AF7A'},
+        {vert:Vertices.fromPath('0 0  50 0  50 50 100 50 100 100 0 100'), color:'#4DC4FF'},
+        {vert:Vertices.fromPath('0 0  50 0  50 100 0 100'), color:'#AA4499'},
+        {vert:Vertices.fromPath('0 0  50 0  50 50  100 50  100 0  150 0  150 100  0 100'), color:'#FFF100'},
     ];
     /**
      * 0 小さい正方形
@@ -67,35 +77,30 @@ export class Game {
      * 5 凹
      */
     const layout = [
-        {id: 0, rot:0, x: 0, y:0, color:'#FF6B22'},
-        {id: 1, rot:0, x: 1, y:0, color:'#005AFF'},
-        {id: 2, rot:0, x: 1, y:1, color:'#03AF7A'},
-        {id: 3, rot:0, x: 1, y:2, color:'#4DC4FF'},
-        {id: 1, rot:0, x: -1, y:2, color:'#005AFF'},
-        {id: 4, rot:0, x: -1, y:3, color:'#FFF100'},
-        {id: 5, rot:0, x: 3, y:5, color:'#990099'},
-        {id: 0, rot:0, x: 1, y:3, color:'#FF6B22'},
+        {id: 0, rot:0, x: 0, y:0},
+        {id: 1, rot:0, x: 1, y:0},
+        {id: 2, rot:0, x: 1, y:1},
+        {id: 3, rot:0, x: 1, y:2},
+        {id: 1, rot:0, x: -1, y:2},
+        {id: 4, rot:0, x: -1, y:3},
+        {id: 5, rot:0, x: 3, y:5},
+        {id: 0, rot:0, x: 1, y:3},
+        {id: 1, rot:0, x: -1, y:6},
+        {id: 1, rot:0, x: 1, y:6},
+        {id: 4, rot:0, x: 2, y:7},
+        {id: 5, rot:0, x: 2, y:9},
+        {id: 0, rot:0, x: 0, y:7},
     ];
 
-    /*
-    var stack = Composites.stack(300, 600 - size * 6/2, 5, 5, 0, 0, function(x, y) {
-        var partA = Bodies.rectangle(x, y, size, size, { label: 'block', density: 0.5, restitution: 0.000000001 });
-
-        return Body.create({
-            parts: [partA]
-        });
-    });*/
     
     var blocks = [];
     layout.forEach(b => {
         let tmpBody = Bodies.fromVertices(
-            0,
-            0, 
-            blockTemplates[b.id], 
+            0, 0, blockTemplates[b.id].vert, 
             {
                 isStatic: false,
                 render: {
-                    fillStyle: b.color
+                    fillStyle: blockTemplates[b.id].color
                 }
             },
             true);
@@ -104,7 +109,7 @@ export class Game {
         let x = tmpBody.vertices[0].x;
         let y = tmpBody.vertices[0].y;
         //Body.translate(tmpBody, {x: 400-x, y: 200-y});
-        Body.translate(tmpBody, {x: 200-x+b.x * size, y: 230-y+b.y * size});
+        Body.translate(tmpBody, {x: 200-x+b.x * size, y: 180-y+b.y * size});
         blocks.push(tmpBody);      
     });
     console.log(blocks);
@@ -115,7 +120,7 @@ export class Game {
     friction: 8, // 摩擦
     label: 'facebook',
     angle: Math.random() * 10,*/
-    let hex = Bodies.polygon(200, 100, 6, 60, 
+    let hex = Bodies.polygon(250, 100, 6, 60, 
         {
             label: 'hex', 
             density: 0.000005, restitution: 0.000000001,
